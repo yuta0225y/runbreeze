e73138d767a73d020e4459c6b7687da4c6faea61
yuta <yas0225y@gmail.com>
2024-10-30 16:42:57 +0900

    deviseの導入及び基本設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # Deviseのストロングパラメータを許可
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # Deviseのストロングパラメータを設定
+  def configure_permitted_parameters
+    # サインアップ時にusernameを許可
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+
+    # アカウント更新時にusernameを許可
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
