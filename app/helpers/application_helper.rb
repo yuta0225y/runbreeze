22dabf7dcd40483c506d15d26af7b7513dd31c65
yuta <yas0225y@gmail.com>
2024-11-01 18:28:35 +0900

    スタイリング、i18の修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def flash_background_class(type)
+    case type.to_sym
+    when :notice then "alert-info"
+    when :success then "alert-success"
+    when :alert  then "alert-error"
+    when :warning then "alert-warning"
+    else "alert-info"
+    end
+  end
 end
