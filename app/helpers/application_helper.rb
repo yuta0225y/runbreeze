4d98f6647f2ef4e1d2f766723235dad420d28e5c
yuta <yas0225y@gmail.com>
2024-11-01 17:50:04 +0900

    フラッシュメッセージの作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_background_color(type)
+    case type.to_sym
+    when :notice then "bg-[#8DBA30]"
+    when :alert  then "bg-[#f84c08]"
+    when :error  then "bg-[#f6b827]"
+    else "bg-gray-500"
+    end
+  end
 end
