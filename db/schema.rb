204db98221a1cbf8630911677984385e6e3b1f53
yuta <yas0225y@gmail.com>
2024-11-02 16:39:51 +0900

    カテゴリーモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_02_020037) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_02_073325) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.bigint "user_id", null: false
