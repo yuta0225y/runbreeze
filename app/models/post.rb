3ba67eb24ae5916850d7c6ab596d2c4f9321a4d9
yuta <yas0225y@gmail.com>
2024-11-05 16:17:56 +0900

    Postモデルにバリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ApplicationRecord
   belongs_to :category
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
+
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :content, presence: true,length: { maximum: 1000 }
+  validates :category_id, presence: true
 end
