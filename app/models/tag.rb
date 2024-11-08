6c9a5a4c5660a34a4fcc36e98ea06d68c944d85e
yuta <yas0225y@gmail.com>
2024-11-08 20:32:39 +0900

    enumを追加

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,6 @@
 class Tag < ApplicationRecord
-  belongs_to :category
+  belongs_to :category, optional: true
+  enum tag_type: { standard: 0, category_specific: 1 }
   has_many :post_tags, dependent: :destroy
   has_many :posts, through: :post_tags
 end
