2395bc13ca3b46dd4bd0e79cfb2ba011ff2a7c77
yuta <yas0225y@gmail.com>
2024-11-14 10:34:09 +0900

    Postモデルにコメントとの関連付けとバリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   belongs_to :category
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 30 }
   validates :content, presence: true, length: { maximum: 1000 }
