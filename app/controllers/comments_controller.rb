9b5cb37b97ee36d3f6a9e9e0f18eb683c6b11dfb
yuta <yas0225y@gmail.com>
2024-11-14 10:35:38 +0900

    コメントの作成を処理するCommentsControllerを作成

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user # 現在のユーザーをコメントの作成者として設定
+
+    if @comment.save
+      redirect_to post_path(@post), notice: 'コメントが追加されました。'
+    else
+      redirect_to post_path(@post), alert: 'コメントの追加に失敗しました。'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
