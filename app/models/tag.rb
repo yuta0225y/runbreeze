b9e5c4aa48dd2a675f2a6f010612bacaf0d6c7ba
yuta <yas0225y@gmail.com>
2024-11-15 18:19:21 +0900

    feat: Tagモデルに検索可能な属性を追加

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,4 +4,9 @@ class Tag < ApplicationRecord
   has_many :posts, through: :post_tags
 
   enum tag_type: { standard: 0, category_specific: 1 }
+  
+  # Ransackで検索可能なカラム
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name]
+  end
 end
