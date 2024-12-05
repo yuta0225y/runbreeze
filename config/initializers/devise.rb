823f3d3ae7728da25f02d0f3510d822b7c4fc325
yuta <yas0225y@gmail.com>
2024-12-05 11:54:33 +0900

    Google認証用のルーティングとDevise設定を追加

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,7 +272,11 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-
+# OmniAuthの設定を追加
+  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
+    scope: 'userinfo.email, userinfo.profile',
+    prompt: 'select_account',
+  }
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
