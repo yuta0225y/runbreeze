e373bfb71a11ba2cf2d85971ff78035b072cb7de
yuta <yas0225y@gmail.com>
2024-12-12 14:48:58 +0900

    開発環境でletter_openerを使用するようActionMailer設定を更新

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,22 @@ Rails.application.configure do
   # caching is enabled.
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  # SMTPによるメール送信の設定
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'localhost',
+    user_name: ENV['MAILER_SENDER'],
+    password: ENV['MAILER_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
+  # Letter Openerを使った開発環境でのメール確認設定
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
