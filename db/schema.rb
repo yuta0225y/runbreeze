0fbd768e508103d86d9ad74382c6c6a674d6cd7b
yuta <yas0225y@gmail.com>
2024-12-23 14:37:36 +0900

    スキーマを更新しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_18_093535) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_22_111049) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "advices", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "input"
+    t.text "response"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_advices_on_user_id"
+  end
 
   create_table "bookmarks", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -107,6 +116,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_18_093535) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "advices", "users"
   add_foreign_key "bookmarks", "posts"
   add_foreign_key "bookmarks", "users"
   add_foreign_key "comments", "posts"
