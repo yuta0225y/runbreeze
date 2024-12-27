f7a014b47e02250eba92ad4fa27f3a7f76ddd6b0
yuta <yas0225y@gmail.com>
2024-12-27 17:03:46 +0900

    RSpec専用のCI設定に変更

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -43,6 +43,9 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  # FactoryBotのメソッドをテスト内で使用可能にする
+  config.include FactoryBot::Syntax::Methods
+
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
