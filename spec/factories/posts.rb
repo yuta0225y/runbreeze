4284163c1f2d4c2a62e2a40b6131bd7498ba9cbf
yuta <yas0225y@gmail.com>
2025-01-06 10:52:18 +0900

    feat: 投稿モデルのファクトリを作成

---
+++ spec/factories/posts.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+    factory :post do
+      title { "テストタイトル" }
+      content { "テストコンテンツ" }
+      status { :draft } 
+  
+      association :user
+      association :category
+  
+      trait :published do
+        status { :published }
+      end
+    end
+  end
+  
