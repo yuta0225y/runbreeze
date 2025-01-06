16074e849b34cb2ff3a081b59c9a1c777031a1af
yuta <yas0225y@gmail.com>
2025-01-06 09:33:59 +0900

    Userモデルのバリデーションおよび関連付けを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,15 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable,
-  :omniauthable, omniauth_providers: %i[google_oauth2]
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[google_oauth2]
 
-  # 一般的なバリデーション（プロフィール更新時のみ適用）
+  # Usernameに関するカスタムバリデーション
   validates :username, presence: true,
                        uniqueness: { case_sensitive: false },
-                       length: { minimum: 3, maximum: 25 },
-                       on: :update_profile
+                       length: { minimum: 3, maximum: 25 }
 
-  validates :email, presence: true,
-                    uniqueness: true,
-                    format: { with: URI::MailTo::EMAIL_REGEXP },
-                    on: :update_profile
-
-  validates :password, length: { minimum: 4 }, if: -> { new_record? || changes[:encrypted_password] }
-  validates :password_confirmation, presence: true, if: -> { password.present? }
-  validates :running_goal, inclusion: { in: [ "ダイエット", "記録更新", "健康維持", "ストレス解消", "体力向上", "仲間づくり" ] }, allow_blank: true
+  # その他のカスタムバリデーション
+  validates :running_goal, inclusion: { in: ["ダイエット", "記録更新", "健康維持", "ストレス解消", "体力向上", "仲間づくり"] }, allow_blank: true
   validates :running_specs, length: { maximum: 50 }, allow_blank: true
   validates :twitter_url, format: { with: URI::DEFAULT_PARSER.make_regexp }, allow_blank: true
   validates :instagram_url, format: { with: URI::DEFAULT_PARSER.make_regexp }, allow_blank: true
@@ -68,21 +61,16 @@ class User < ApplicationRecord
 
   # OmniAuthからのユーザー取得または作成
   def self.from_omniauth(auth)
-    # まず provider と uid でユーザーを検索
     user = find_by(provider: auth.provider, uid: auth.uid)
 
     if user
-      # 既存のユーザーが見つかった場合、そのユーザーを返す
       user
     else
-      # email でユーザーを検索
       user = find_by(email: auth.info.email)
       if user
-        # 既存のユーザーに provider と uid を関連付けて保存
         user.update(provider: auth.provider, uid: auth.uid)
         user
       else
-        # ユーザー名のユニーク性を確保
         base_username = auth.info.name.parameterize
         unique_username = base_username
         counter = 1
@@ -91,7 +79,6 @@ class User < ApplicationRecord
           counter += 1
         end
 
-        # 新規ユーザーを作成
         create(
           email: auth.info.email,
           username: unique_username,
